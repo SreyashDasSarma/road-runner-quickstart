a4fbaf1421a6ce34df8bff18629f05de1d20c4ff
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-17 20:06:10 -0700

    Update TuningController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/VeloPID/TuningController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/VeloPID/TuningController.java
@@ -7,11 +7,11 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
 public class TuningController {
-    public static double MOTOR_TICKS_PER_REV = 537.6;
-    public static double MOTOR_MAX_RPM = 312.5;
+    public static double MOTOR_TICKS_PER_REV = 28;
+    public static double MOTOR_MAX_RPM = 6000;
     public static double MOTOR_GEAR_RATIO = 0.5; // output (wheel) speed / input (motor) speed
 
-    public static double TESTING_MAX_SPEED = 0.9 * MOTOR_MAX_RPM;
+    public static double TESTING_MAX_SPEED = 0.5 * MOTOR_MAX_RPM;
     public static double TESTING_MIN_SPEED = 0.3 * MOTOR_MAX_RPM;
 
 
