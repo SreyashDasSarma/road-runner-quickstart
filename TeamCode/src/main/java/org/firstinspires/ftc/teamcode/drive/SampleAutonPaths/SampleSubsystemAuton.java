0bbd06792d7d960778f162857eec5ce58be0d6d3
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-18 17:29:26 -0700

    Update SampleSubsystemAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/SampleSubsystemAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/SampleSubsystemAuton.java
@@ -1,22 +1,33 @@
 package org.firstinspires.ftc.teamcode.drive.SampleAutonPaths;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
  * This is an example of a more complex path to really test the tuning.
  */
-@Autonomous(group = "drive")
+@TeleOp(group = "drive")
 public class SampleSubsystemAuton extends LinearOpMode {
+    public static PIDCoefficients MOTOR_VELO_PID = new PIDCoefficients(0, 0, 0);
+
+    public static double kV = 0.00071;
+    public static double kA = 0;
+    public static double kStatic = 0;
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+
+    private final ElapsedTime veloTimer = new ElapsedTime();
     @Override
     public void runOpMode() throws InterruptedException {
         HardwareFile drive = new HardwareFile(hardwareMap);
-
-        waitForStart();
-        if (isStopRequested()) return;
-        drive.leftIntakeHolder.setPosition(1);
-        drive.rightIntakeHolder.setPosition(0);
-        sleep(2000);
+        //for(double i=0.6;i<0.8;i+=0.05) {
+            drive.tilter.setPosition(0.715);
+            sleep(5000);
+           // telemetry.addData("Pos",i);
+            //telemetry.update();
+        //}
     }
 
 }
