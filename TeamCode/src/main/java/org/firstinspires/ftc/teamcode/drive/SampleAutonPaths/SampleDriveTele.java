71dcc5dada30a4846a417d1aeb332a84b3a2f607
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-22 20:03:08 -0700

    Update SampleDriveTele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/SampleDriveTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/SampleDriveTele.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@TeleOp(name="TwoDrivers", group="Linear Opmode")
+@TeleOp(name="DriverControlJr", group="Linear Opmode")
 public class SampleDriveTele extends LinearOpMode implements Runnable{
     public static HardwareFile robot;
 
@@ -15,7 +15,6 @@ public class SampleDriveTele extends LinearOpMode implements Runnable{
         robot = new HardwareFile(hardwareMap);
         robot.driveTrain=new SampleMecanumDrive(hardwareMap);
         waitForStart();
-        guardpos=false;
         while (opModeIsActive()) {
             run();
         }
@@ -34,58 +33,39 @@ public class SampleDriveTele extends LinearOpMode implements Runnable{
         );
         setMultiplier();
         if(gamepad2.right_bumper) {
-            robot.intakeL.setPower(-1);
-            robot.intakeR.setPower(-1);
-            robot.in1.setPower(1);
-            robot.in2.setPower(1);
+            robot.intakeMotor.setPower(-1);
+            robot.transportMotor.setPower(-1);
         }else if(gamepad2.right_trigger==1){
-            robot.intakeL.setPower(1);
-            robot.intakeR.setPower(1);
-            robot.in1.setPower(-1);
-            robot.in2.setPower(-1);
+            robot.intakeMotor.setPower(1);
+            robot.transportMotor.setPower(1);
         }else{
-            robot.intakeL.setPower(0);
-            robot.intakeR.setPower(0);
-            robot.in1.setPower(0);
-            robot.in2.setPower(0);
+            robot.intakeMotor.setPower(0);
+            robot.transportMotor.setPower(0);
         }
         if(gamepad2.a){
-            robot.arm1.setPosition(0.93);
-            robot.arm2.setPosition (0.07);
+            robot.armWobble.setPosition(0.93);
             sleep(500);
-            robot.grabber.setPosition(0.63);
-            robot.grabber2.setPosition(0.29);
+            robot.grabberWobble.setPosition(0.63);
         }else if(gamepad2.y){
-            robot.grabber.setPosition(0.13);
-            robot.grabber2.setPosition(0.83);
+            robot.grabberWobble.setPosition(0.13);
             sleep(500);
-            robot. arm1.setPosition(0.1);
-            robot.arm2.setPosition (0.88);
+            robot.armWobble.setPosition(0.1);
         }
         if(gamepad2.left_bumper){
-            robot.leftIntakeHolder.setPosition(1);
-            robot.rightIntakeHolder.setPosition(0);
             robot.shooter.setPower(1);
             sleep(500);
             for(int i=0;i<=3;++i){
-                robot.magup();
-                robot.magup();
                 robot.slapper.setPosition(0.35);
                 sleep(100);
                 robot.slapper.setPosition(0.5);
                 sleep(1500);
             }
             robot.shooter.setPower(0);
-            robot.magdown();
         }
         if(gamepad2.dpad_up){
-            robot.leftIntakeHolder.setPosition(1);
-            robot.rightIntakeHolder.setPosition(0);
             robot.shooter.setPower(0.4);
             sleep(500);
             for(int i=0;i<1;++i){
-                robot.magup();
-                robot.magup();
                 sleep(2500);
                 robot.slapper.setPosition(0.35);
                 sleep(100);
@@ -93,12 +73,10 @@ public class SampleDriveTele extends LinearOpMode implements Runnable{
                 sleep(1500);
             }
             robot.shooter.setPower(0);
-            robot.magdown();
         }
         telemetry.update();
         robot.driveTrain.update();
     }
-    public static boolean guardpos;
     private void setMultiplier() {
         if (gamepad1.left_trigger >= 0.3) {
             lxMult = 0.5;
