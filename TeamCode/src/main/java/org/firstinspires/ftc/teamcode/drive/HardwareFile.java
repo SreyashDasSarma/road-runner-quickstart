5487e13818b33be3be01a1870fb4e7bc10cd923d
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-23 21:59:15 -0700

    Update and rename TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAutonPaths/HardwareFile.java to TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/HardwareFile.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/HardwareFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/HardwareFile.java
@@ -1,8 +1,9 @@
-package org.firstinspires.ftc.teamcode.drive.SampleAutonPaths;
+package org.firstinspires.ftc.teamcode.drive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,11 +12,10 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class HardwareFile {
     private LinearOpMode linearOpMode;
-    public DcMotor intakeMotor, shooter;
-    public Servo armWobble;
-    public Servo grabberWobble, slapper, tilter, shooterflap;
+    public DcMotor intakeMotor, shooter, transportMotor, armWobble;
+    public Servo grabberWobble, slapper, tilter;
     HardwareMap map;
-    public SampleMecanumDrive driveTrain;
+    //public SampleMecanumDrive driveTrain;
     public static Pose2d robotPose = new Pose2d();
 
     public HardwareFile(HardwareMap imported) {
@@ -25,32 +25,33 @@ public class HardwareFile {
     private void construct(HardwareMap imported){
         map = imported;
         intakeMotor = map.get(DcMotor.class, "intakeR");
-        shooter = map.get(DcMotor.class, "fw");
-        armWobble = map.get(Servo.class, "wobbleArm2");
+        transportMotor = map.get(DcMotor.class, "intakeL");
+        /*shooter = map.get(DcMotor.class, "fw");
+        armWobble = map.get(DcMotor.class, "wobbleArm2");
         grabberWobble = map.get(Servo.class, "wobbleGrabber2");
         slapper = map.get(Servo.class, "mag");
         tilter = map.get(Servo.class, "tilt");
-        shooterflap = map.get(Servo.class, "flap");
+        armWobble.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);*/
 
     }
-    public void wobbleArmUp() {
-        armWobble(0.88);
+    /*public void wobbleArmUp() {
+        armWobble.setTargetPosition(100);
     }
     public void wobbleArmDown() {
-        armWobble (0.07);
+        armWobble.setTargetPosition(100);
 
     }
     public void wobbleArmVertical(){
-        armWobble(0.5);
+        armWobble.setTargetPosition(100);
     }
     public void grab(){
-        grabberWobble(0.83);
+        grabberWobble.setPosition(0.83);
     }
     public void release(){
-        grabberWobble(0.29);
-    }
+        grabberWobble.setPosition(0.29);
+    }*/
     public void intake(double intakeSpeed){
-        intakeMotor(-intakeSpeed);
+        intakeMotor.setPower(-intakeSpeed);
     }
 
     public void shooter(double shooterpower){
