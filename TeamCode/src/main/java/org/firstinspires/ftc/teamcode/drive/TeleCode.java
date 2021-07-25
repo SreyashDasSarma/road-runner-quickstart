ea6138caa9f46b2901ad578227a5cd08419c96f8
maartoon <85377718+maartoon@users.noreply.github.com>
2021-07-24 19:36:04 -0700

    Update TeleCode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleCode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.drive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -32,6 +33,7 @@ public class TeleCode extends LinearOpMode implements Runnable{
                 )
         );*/
         //setMultiplier();
+        //INTAKE
         if(gamepad2.right_bumper) {
             robot.intakeMotor.setPower(-0.6);
             robot.transportMotor.setPower(0.6);
@@ -42,42 +44,46 @@ public class TeleCode extends LinearOpMode implements Runnable{
             robot.intakeMotor.setPower(0);
             robot.transportMotor.setPower(0);
         }
-        /*if(gamepad2.a){
-            robot.armWobble.setTargetPosition(93);
-            sleep(500);
-            robot.grabberWobble.setPosition(0.63);
+        //WOBBLE
+        if(gamepad2.a){
+            angleToPosition(0);
+//            sleep(500);
+//            robot.grabberWobble.setPosition(0.63);
         }else if(gamepad2.y){
-            robot.grabberWobble.setPosition(0.13);
-            sleep(500);
-            robot.armWobble.setTargetPosition(1);
+//            robot.grabberWobble.setPosition(90);
+//            sleep(500);
+            angleToPosition(90);
         }
-        if(gamepad2.left_bumper){
-            robot.shooter.setPower(1);
-            sleep(500);
-            for(int i=0;i<=3;++i){
-                robot.slapper.setPosition(0.35);
-                sleep(100);
-                robot.slapper.setPosition(0.5);
-                sleep(1500);
-            }
-            robot.shooter.setPower(0);
+        //SHOOTER
+       if(gamepad2.right_trigger > 0){
+//            robot.shooter.setPower(1);
+//            sleep(500);
+//            for(int i=0;i<=3;++i){
+                robot.shooterpush.setPosition(0.4);
+//                sleep(100);
+//                robot.shooterpush.setPosition(0.85);
+//                sleep(1500);
+//            }
+//            robot.shooter.setPower(0);
         }
-        if(gamepad2.dpad_up){
-            robot.shooter.setPower(0.4);
-            sleep(500);
-            for(int i=0;i<1;++i){
-                sleep(2500);
-                robot.slapper.setPosition(0.35);
-                sleep(100);
-                robot.slapper.setPosition(0.5);
-                sleep(1500);
-            }
-            robot.shooter.setPower(0);
+        if(gamepad2.left_trigger > 0){
+//            robot.shooter.setPower(0.4);
+//            sleep(500);
+//            for(int i=0;i<1;++i){
+//                sleep(2500);
+//                robot.shooterpush.setPosition(0.4);
+//                sleep(100);
+                robot.shooterpush.setPosition(0.85);
+//                sleep(1500);
+//            }
+//            robot.shooter.setPower(0);
         }
         telemetry.update();
-        robot.driveTrain.update();*/
+//        robot.driveTrain.update();
     }
-    /*private void setMultiplier() {
+
+    private void setMultiplier() {
+        //MULTIPLIERS
         if (gamepad1.left_trigger >= 0.3) {
             lxMult = 0.5;
             rxMult = 0.5;
@@ -93,5 +99,15 @@ public class TeleCode extends LinearOpMode implements Runnable{
             lxMult = -lxMult;
             lyMult = -lyMult;
         }
-    }*/
+    }
+    public void angleToPosition(double angle){
+        int target = (int)((angle/360)*537);
+        robot.armWobble.setTargetPosition(target);
+        robot.armWobble.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.armWobble.setPower(0.25);
+
+        while (robot.armWobble.getCurrentPosition() != target) {
+        }
+        robot.armWobble.setPower(0);
+    }
 }
