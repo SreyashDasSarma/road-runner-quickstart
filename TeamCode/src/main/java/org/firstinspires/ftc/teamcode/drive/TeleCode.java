5a6f76a34a9870a21b88733f2f488a4c5a241c12
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-08-06 22:20:49 -0700

    Update TeleCode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleCode.java
@@ -54,9 +54,9 @@ public class TeleCode extends LinearOpMode implements Runnable{
             robot.shooter.setPower(1);
             sleep(500);
             for(int i=0;i<=3;++i){
-                robot.slapper.setPosition(0.35);
+                robot.shooterpush.setPosition(0.35);
                 sleep(100);
-                robot.slapper.setPosition(0.5);
+                robot.shooterpush.setPosition(0.5);
                 sleep(1500);
             }
             robot.shooter.setPower(0);
@@ -66,9 +66,9 @@ public class TeleCode extends LinearOpMode implements Runnable{
             sleep(500);
             for(int i=0;i<1;++i){
                 sleep(2500);
-                robot.slapper.setPosition(0.35);
+                robot.shooterpush.setPosition(0.35);
                 sleep(100);
-                robot.slapper.setPosition(0.5);
+                robot.shooterpush.setPosition(0.5);
                 sleep(1500);
             }
             robot.shooter.setPower(0);
